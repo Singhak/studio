c0d8f398b72d569649994462b8f1504480b0d8ca
singhak <anilsingh.singh09@gmail.com>
2025-07-07 08:05:15 +0530

    add api url

--- src/lib/apiUtils.ts
+++ src/lib/apiUtils.ts
@@ -2,7 +2,7 @@
 const CUSTOM_ACCESS_TOKEN_KEY = 'courtlyCustomAccessToken';
 
 export function getApiBaseUrl(): string {
-  let baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
+  let baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://game-nest-singhaks-projects.vercel.app/';
   if (baseUrl.endsWith('/')) {
     baseUrl = baseUrl.slice(0, -1);
   }
