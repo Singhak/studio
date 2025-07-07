d4cb6978801a80da1a95ff028554e03b08a0d747
singhak <anilsingh.singh09@gmail.com>
2025-07-07 17:44:12 +0530

    change for vercel

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
+  // output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    unoptimized: true,
+    // unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
