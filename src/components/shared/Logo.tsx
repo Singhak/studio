43eac17f3b95d043a6f2cf674c314956439ddda3
Anil Singh <anilsingh.singh09@gmail.com>
2025-07-19 11:07:21 +0000

    complete logo is not appeared

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -17,42 +17,27 @@ export function Logo({ className }: LogoProps) {
         xmlns="http://www.w3.org/2000/svg"
         className="h-8 w-auto"
       >
-      <defs>
-        <linearGradient id="grad-blue" x1="0" y1="0" x2="1" y2="1">
-          <stop offset="0%" stopColor="#0052D4" />
-          <stop offset="100%" stopColor="#4364F7" />
-        </linearGradient>
-        <linearGradient id="grad-green" x1="0" y1="0" x2="1" y2="1">
-          <stop offset="0%" stopColor="#1CB5E0" />
-          <stop offset="100%" stopColor="#0072FF" />
-        </linearGradient>
-        <linearGradient id="grad-orange" x1="0" y1="0" x2="1" y2="1">
-          <stop offset="0%" stopColor="#F89B29" />
-          <stop offset="100%" stopColor="#FF4E50" />
-        </linearGradient>
-        <path id="p-shape" d="M26.9,23.38c-3.7,0-6.7-3-6.7-6.7s3-6.7,6.7-6.7h11.4v-5.2H26.9C17.58,4.78,10,12.36,10,21.68 c0,9.32,7.58,16.9,16.9,16.9h11.4v-5.2H26.9V23.38z" />
-        <clipPath id="p-clip">
-            <use xlinkHref="#p-shape"/>
-        </clipPath>
-      </defs>
-      
-      <g>
-        <path d="M26.47,3.15C16.8,3.15,9,10.95,9,20.62c0,9.67,7.8,17.47,17.47,17.47h12.53V30.52H26.47V24.5h12.53v-7.8H26.47V3.15z M26.47,24.5V30.52" fill="none"/>
-        <path d="M23.1,3.15H39V16.7H23.1z" fill="#005C97"/>
-        <path d="M26.47,30.52h12.53v-6.02H26.47z" fill="#34E89E"/>
-        <path d="M9,20.62c0-9.67,7.8-17.47,17.47-17.47" fill="none"/>
-        <path d="M26.47,3.15h12.53v13.55c0,4.3-3.48,7.8-7.8,7.8h-4.73V3.15z M26.47,16.7h4.73c1.55,0,2.8-1.25,2.8-2.8V8.15h-7.53V16.7z" fill="#0072FF"/>
-        <path d="M26.47,38.09c9.67,0,17.47-7.8,17.47-17.47h-12.5V30.52h-4.97c-9.67,0-17.47-7.8-17.47-17.47" fill="none" />
-        <path d="M23.1,16.7v13.82h3.37c9.67,0,17.47-7.8,17.47-17.47V16.7H23.1z" fill="#0A8742" />
+        <g>
+          {/* Main 'P' shape base */}
+          <path d="M26.47,3.15C16.8,3.15,9,10.95,9,20.62c0,9.67,7.8,17.47,17.47,17.47h12.53V30.52H26.47V24.5h12.53v-7.8H26.47V3.15z" fill="none"/>
+          
+          {/* Blue top-left quadrant of P */}
+          <path d="M23.1,3.15h12.53v13.55c0,4.3-3.48,7.8-7.8,7.8h-4.73V3.15z" fill="#0072FF"/>
+          
+          {/* Lighter Green bottom-right quadrant of P */}
+          <path d="M26.47,30.52h12.53v-6.02H26.47z" fill="#34E89E"/>
+          
+          {/* Darker Green bottom-left quadrant of P */}
+          <path d="M23.1,16.7v13.82h3.37c9.67,0,17.47-7.8,17.47-17.47V16.7H23.1z" fill="#0A8742"/>
 
-        {/* Swoosh */}
-        <path d="M30,13c5,2,8,7,6,12s-7,8-12,6s-8-7-6-12S25,11,30,13z" fill="url(#grad-orange)"/>
-        
-        {/* Text */}
-        <text x="48" y="24" fontFamily="Arial, sans-serif" fontSize="18" fontWeight="bold" fill="hsl(var(--primary))">
-          Playce
-        </text>
-      </g>
+          {/* Orange swoosh */}
+          <path d="M30,13c5,2,8,7,6,12s-7,8-12,6s-8-7-6-12S25,11,30,13z" fill="#FF4E50"/>
+          
+          {/* Text "Playce" */}
+          <text x="48" y="24" fontFamily="-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji'" fontSize="18" fontWeight="bold" fill="hsl(var(--primary))">
+            Playce
+          </text>
+        </g>
       </svg>
     </Link>
   );
